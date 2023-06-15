7b0a70c12196038c2f27750e1547f6575a4a36b2
siscal-gs <57602933+siscal-gs@users.noreply.github.com>
2023-06-15 21:33:43 +0200

    commit

---
+++ android/app/src/main/kotlin/com/siscal/flashchat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.siscal.flashchat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flashchat/screens/welcome_screen.dart';
+import 'package:flashchat/screens/login_screen.dart';
+import 'package:flashchat/screens/registration_screen.dart';
+import 'package:flashchat/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  // Ensure that Firebase is initialized
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize Firebase
+  await Firebase.initializeApp();
+  //
+  runApp(FlashChat());
+}
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
+    );
+  }
+}
